e40c4ab66299e7c638ed55a6df310d3258ffd301
AnnoyedArt1256 <66080161+AnnoyedArt1256@users.noreply.github.com>
2022-01-01 20:10:43 +0300

    Create repl_355b.js

---
+++ repl_355b.js
@@ -0,0 +1,1 @@
+r=require("readline").createInterface({input:process.stdin,output:process.stdout});function prompt(){return new Promise(e=>{r.question("> ",o=>{"exit"==o&&process.exit(1);try{void 0!=(0,eval)(o)&&console.log((0,eval)(o))}catch(e){console.log(e.toString().split("\n")[0])}"clear"!=o&&"cls"!=o||console.clear(),e()})})}(async e=>{for(;;)await prompt()})();
