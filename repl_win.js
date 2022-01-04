518fbebff318d2ab03c8c773eabb6eae27862fc8
AnnoyedArt1256 <66080161+AnnoyedArt1256@users.noreply.github.com>
2022-01-04 12:10:27 +0300

    Update repl_win.js

--- repl_win.js
+++ repl_win.js
@@ -1,7 +1,7 @@
 process.stdout.write("> ")
 process.stdin.on('data', p => {
   o = p.toString().slice(0,-1);
-  o = o.charCodeAt(o.length-1)<13?o.slice(0,-1):o;
+  o = o.charCodeAt(o.length-1)<14?o.slice(0,-1):o;
   if (o == "exit") { process.exit() }
   try {
       if ( (1, eval)(o) != undefined) console.log( (1, eval)(o));
